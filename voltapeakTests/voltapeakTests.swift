d0cb00558f3d16824cd54c0095a1bc620e12b6bb
Stéphane Cadinot <scadinot@trace.fr>
2026-05-11 19:00:34 +0200

    tests

---
+++ voltapeakTests/voltapeakTests.swift
@@ -0,0 +1,16 @@
+//
+//  voltapeakTests.swift
+//  voltapeakTests
+//
+//  Created by Stéphane Cadinot on 11/05/2026.
+//
+
+import Testing
+
+struct voltapeakTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
